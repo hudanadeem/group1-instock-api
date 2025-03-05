625bcc629141157a2a3d3bc37eade865f7060e58
“Mimta <mimta.chowdhury@gmail.com>
2025-03-05 16:42:47 -0500

    create a function on the back-end using Express and Express Router to provide the front-end with a single warehouse’s details

--- routes/warehouses-routes.js
+++ routes/warehouses-routes.js
@@ -6,12 +6,10 @@ const warehousesRouter = express.Router();
 warehousesRouter.route("/")
 .get(warehouseController.getAllWarehouses);
 
-//   .get(warehouseController.getWarehouse)
 //   .put(warehouseController.editWarehouse)
-//   .delete(warehouseController.deleteWarehouse);
 
 warehousesRouter.route("/:id")
-    // .get(warehouseController.getWarehouse)  
+    .get(warehouseController.getWarehouse)  
     .delete(warehouseController.deleteWarehouse);
 
 export default warehousesRouter;
