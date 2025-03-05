9808d10e8e77b19ce975a4e708ad2f32608576d0
ozokolosandra <ozokolosandra@gamil.com>
2025-03-05 08:38:29 -0500

    Created GET Request Endpoint to list of warehouses

--- routes/inventories-routes.js
+++ routes/inventories-routes.js
@@ -1,3 +1,7 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
 import express from "express";
 
 const InventoryRouter = express.Router();
--- routes/warehouses-routes.js
+++ routes/warehouses-routes.js
@@ -1,6 +1,13 @@
 import express from "express";
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+import * as warehouseController from "../controllers/warehouses-controller.js"
 
 const warehousesRouter = express.Router();
+
+warehousesRouter.route("/")
+.get(warehouseController.index)
 
 
 export default warehousesRouter;
